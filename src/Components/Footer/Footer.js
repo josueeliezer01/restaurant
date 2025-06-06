3b8922779ba63997b6c1c46b32454144f21e92c2
josueeliezer01 <eliezercasstilho@gmail.com>
2025-06-06 16:40:54 +0100

    responsive bug fixes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,7 +8,9 @@ const Footer = () => {
       <div className="footer-content">
         {/* Logo */}
         <div className="logo">
-          <Link to="/" aria-label="Página Inicial">
+          <Link
+            to="/"
+            aria-label="Página Inicial">
             <Logo />
           </Link>
         </div>
@@ -26,8 +28,7 @@ const Footer = () => {
             aria-label="Facebook"
             target="_blank"
             rel="noopener noreferrer"
-            className="social-icon"
-          >
+            className="social-icon">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               shape-rendering="geometricPrecision"
@@ -36,8 +37,7 @@ const Footer = () => {
               fillRule="evenodd"
               clipRule="evenodd"
               viewBox="0 0 509 509"
-              className="icon-facebook"
-            >
+              className="icon-facebook">
               <g fillRule="nonzero">
                 <path
                   fill="#0866FF"
@@ -55,8 +55,7 @@ const Footer = () => {
             aria-label="TripAdvisor"
             target="_blank"
             rel="noopener noreferrer"
-            className="social-icon"
-          >
+            className="social-icon">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 1333.31 1333.31"
@@ -65,10 +64,14 @@ const Footer = () => {
               image-rendering="optimizeQuality"
               fillRule="evenodd"
               clipRule="evenodd"
-              className="icon-tripadvisor"
-            >
+              className="icon-tripadvisor">
               <g fillRule="nonzero">
-                <circle cx="666.66" cy="666.66" r="666.66" fill="#34e0a1" />
+                <circle
+                  cx="666.66"
+                  cy="666.66"
+                  r="666.66"
+                  fill="#34e0a1"
+                />
                 <path d="M1078.42 536.6l80.45-87.52h-178.4c-89.31-61.01-197.17-96.54-313.81-96.54-116.5 0-224.06 35.61-313.22 96.54H174.6l80.44 87.52c-49.31 44.99-80.22 109.8-80.22 181.75 0 135.79 110.09 245.88 245.88 245.88 64.51 0 123.27-24.88 167.14-65.55l78.81 85.81 78.81-85.73c43.87 40.67 102.57 65.47 167.07 65.47 135.79 0 246.03-110.09 246.03-245.88.07-72.03-30.84-136.83-80.15-181.75zM420.77 884.75c-91.92 0-166.4-74.48-166.4-166.4s74.49-166.4 166.4-166.4c91.92 0 166.4 74.49 166.4 166.4 0 91.91-74.49 166.4-166.4 166.4zm245.96-171.24c0-109.5-79.63-203.5-184.73-243.65 56.84-23.76 119.18-36.94 184.66-36.94 65.47 0 127.89 13.18 184.73 36.94-105.02 40.23-184.65 134.15-184.65 243.65zm245.88 171.24c-91.92 0-166.4-74.48-166.4-166.4s74.49-166.4 166.4-166.4c91.92 0 166.4 74.49 166.4 166.4 0 91.91-74.49 166.4-166.4 166.4zm0-253.7c-48.2 0-87.23 39.03-87.23 87.23 0 48.19 39.03 87.22 87.23 87.22 48.19 0 87.22-39.03 87.22-87.22 0-48.12-39.03-87.23-87.22-87.23zM508 718.35c0 48.19-39.03 87.22-87.23 87.22-48.19 0-87.22-39.03-87.22-87.22 0-48.2 39.03-87.23 87.22-87.23 48.19-.07 87.23 39.03 87.23 87.23z" />
               </g>
             </svg>
@@ -77,7 +80,10 @@ const Footer = () => {
       </div>
 
       <div className="footer-bottom">
-        <p>© {new Date().getFullYear()} Quinta da Magarenha. Todos os direitos reservados.</p>
+        <p>
+          © {new Date().getFullYear()} Quinta da Magarenha. Todos os direitos
+          reservados.
+        </p>
       </div>
     </footer>
   );
